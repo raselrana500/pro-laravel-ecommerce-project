4b554157f1fceb2861e115005e79005f03add83a
raselrana500 <raselrana500@gmail.com>
2021-09-11 02:56:07 +0600

    mastaring frontend

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 
 
 
-Route::get('/', function () {return view('layouts.app');});
+Route::get('/', function () {return view('pages.index');});
 //auth & user
 Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
