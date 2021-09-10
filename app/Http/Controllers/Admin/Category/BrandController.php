88acec7d6b606091894317b41472fffe7bf6a102
raselrana500 <raselrana500@gmail.com>
2021-09-10 23:39:20 +0600

    first commit

---
+++ app/Http/Controllers/Admin/Category/BrandController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers\Admin\Category;
+
+use App\Brand;
+use Illuminate\Http\Request;
+
+class BrandController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth:admin');
+    }
+    
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Brand  $brand
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Brand $brand)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Brand  $brand
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Brand $brand)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Brand  $brand
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Brand $brand)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Brand  $brand
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Brand $brand)
+    {
+        //
+    }
+}
